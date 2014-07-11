b83f4cf21f422a4d7beef5d14ddfdbfe50f455e5
parthiban <baluparthiban1989@gmail.com>
2014-07-11 11:24:13 +0530

    new structure
    we have added a new structure.

---
+++ Source/TrainingSolution.Xamarin/TrainingSolution.Droid/HomeActivity.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TrainingSolution.Droid
+{
+	[Activity (Label = "HomeActivity")]			
+	public class HomeActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Home);
+
+			// Create your application here
+		}
+
+
+		public  override void OnBackPressed ()
+		{
+			//base.OnBackPressed ();
+			onBackLogout ();
+		}
+
+		void onBackLogout()
+
+		{
+			var builder = new AlertDialog.Builder(this);
+			builder.SetMessage(Resource.String.dialog_title);
+			builder.SetTitle("Exit");
+			//builder.SetIcon(Resource.Drawable.logout);
+			builder.SetPositiveButton(Resource.String.yes, (s, e) => 
+				{ 
+					var intent = new Intent(this, typeof(MainActivity));
+					intent.SetFlags(ActivityFlags.ClearTop); 
+					StartActivity(intent);
+					this.Finish();
+				});
+
+			builder.SetNegativeButton(Resource.String.no, (s, e) => { }).Create();
+			builder.Show();
+		}
+	}
+}
+
---
+++ Source/TrainingSolution.Xamarin/TrainingSolution.Droid/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TrainingSolution.Droid
+{
+	[Activity (Label = "TRU Training", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+
+			button.Click += delegate {
+
+				//This is place to navigate one screen other screen by firing intent.//
+
+				var homeActivity = new Intent(this, typeof(HomeActivity));
+				StartActivity(homeActivity);
+
+				Toast.MakeText(this, "Navigated to Home Screen", ToastLength.Short).Show(); 
+			};
+		}
+
+	}
+}
+
+
