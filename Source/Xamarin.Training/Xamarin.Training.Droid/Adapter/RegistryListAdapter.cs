fdf6cffc758ec7f266a8d78088386f5c7f27f213
Nithin Warrier <nithin.warier@happiestminds.com>
2014-07-15 18:29:49 +0530

    Added Registry management files for registry mgmt use case.

---
+++ Source/Xamarin.Training/Xamarin.Training.Droid/Adapter/RegistryListAdapter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Android.Widget;
+using Android.Views;
+using Android.App;
+
+/**
+ * @Author Nithin
+ * @Date Jul 15, 2014
+ */
+
+namespace Xamarin.Training.Droid
+{
+	public class RegistryListAdapter : BaseAdapter
+	{
+
+		/**
+	 	* Holder class to hold the widgets for each list item
+	 	*/
+
+		/*static class ViewHolder {
+			ImageView ivRetailerLogo;
+			TextView tvRetailerName;
+		}*/
+
+		public RegistryListAdapter (Activity context) : base() {
+
+		}
+
+
+		public override int Count {
+			get { return 0; }
+		}
+
+		public override Java.Lang.Object GetItem(int position) {
+			return null;
+		}
+
+		public override long GetItemId(int position) {
+			return position;
+		}
+
+		public override View GetView(int position, View convertView, ViewGroup parent) {
+			return null;
+		}
+	}
+}
+
---
+++ Source/Xamarin.Training/Xamarin.Training.Droid/RegistryManagementActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+/**
+ * @Author Nithin
+ * @Date Jul 15, 2014
+ */
+
+namespace Xamarin.Training.Droid
+{
+	[Activity (Label = "RegistryManagementActivity")]	
+	public class RegistryManagementActivity  : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.RegistryManagement);
+
+			// Create your application here
+
+			ListView prodList = FindViewById<ListView>(Resource.Id.productList);
+			//prodList.SetAdapter (new ProductListAdapter(this));
+			prodList.Adapter = new RegistryListAdapter (this);
+
+		}
+	}
+}
+
